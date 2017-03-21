b9de40f04ded51bfb5a349ce33758a9f365e27c4
Mitsuru Y <mitsuruy@reallyenglish.com>
2017-03-21 13:48:16 +0900

    rubocop

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,62 +1,99 @@
-require "spec_helper"
-require "serverspec"
+require 'spec_helper'
+require 'serverspec'
 
-package = "jenkins"
-service = "jenkins"
-user    = "jenkins"
-group   = "jenkins"
-ports   = [ 8080 ]
-log_file = "/var/log/jenkins/jenkins.log"
-home    = "/var/lib/jenkins"
-cli     = "/usr/bin/jenkins-cli.jar"
-url     = "http://127.0.0.1:8080/jenkins"
-plugins = [ "git", "hipchat", "matrix-project" ]
-ssh_passphrase = "passphrase"
-ssh_checksum = "2048 SHA256:L5L3cuQABEnrDP+xNRz9yzAtU9cicM9O0rJTpkMWtpE #{user}@#{host_inventory['fqdn']} (RSA)"
+package = 'jenkins'
+service = 'jenkins'
+user    = 'jenkins'
+group   = 'jenkins'
+ports   = [8080]
+log_file = '/var/log/jenkins/jenkins.log'
+home    = '/var/lib/jenkins'
+cli     = '/usr/bin/jenkins-cli.jar'
+url     = 'http://127.0.0.1:8080/jenkins'
+plugins = ['git', 'hipchat', 'matrix-project']
+jenkins_java_opts =
+  '-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false'
+jenkins_args =
+  '"--prefix=/jenkins --webroot=/usr/local/jenkins/war --httpPort=8180"'
+ssh_passphrase = 'passphrase'
+ssh_checksum =
+  "2048 SHA256:L5L3cuQABEnrDP+xNRz9yzAtU9cicM9O0rJTpkMWtpE\
+ #{user}@#{host_inventory['fqdn']} (RSA)"
 
 case os[:family]
-when "freebsd"
-  ports   = [ 8180 ]
-  home    = "/usr/local/jenkins"
-  cli     = "/usr/local/bin/jenkins-cli.jar"
-  url     = "http://127.0.0.1:8180/jenkins"
-  log_file = "/var/log/jenkins.log"
-when "ubuntu"
-  ssh_checksum = "2048 27:e7:34:1a:33:48:c5:16:df:1c:ce:dc:80:78:39:5f  #{user}@#{host_inventory['fqdn']} (RSA)" if Gem::Version.new(os[:release]) <= Gem::Version.new("14.04")
-when "redhat"
-  ssh_checksum = "2048 27:e7:34:1a:33:48:c5:16:df:1c:ce:dc:80:78:39:5f  #{user}@#{host_inventory['fqdn']} (RSA)" if Gem::Version.new(os[:release]) <= Gem::Version.new("7.3.1611")
+when 'freebsd'
+  ports   = [8180]
+  home    = '/usr/local/jenkins'
+  cli     = '/usr/local/bin/jenkins-cli.jar'
+  url     = 'http://127.0.0.1:8180/jenkins'
+  log_file = '/var/log/jenkins.log'
+when 'ubuntu'
+  jenkins_args =
+    "--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT\
+ --prefix=/jenkins"
+  if Gem::Version.new(os[:release]) <= Gem::Version.new('14.04')
+    ssh_checksum =
+      "2048 27:e7:34:1a:33:48:c5:16:df:1c:ce:dc:80:78:39:5f\
+  #{user}@#{host_inventory['fqdn']} (RSA)"
+  end
+when 'redhat'
+  if Gem::Version.new(os[:release]) <= Gem::Version.new('7.3.1611')
+    ssh_checksum =
+      "2048 27:e7:34:1a:33:48:c5:16:df:1c:ce:dc:80:78:39:5f\
+  #{user}@#{host_inventory['fqdn']} (RSA)"
+  end
 end
 
 describe package(package) do
   it { should be_installed }
-end 
+end
 
 describe file(log_file) do
   it { should be_file }
 end
 
 case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/jenkins") do
+when 'freebsd'
+  describe file('/etc/rc.conf.d/jenkins') do
     it { should be_file }
-    its(:content) { should match(/^jenkins_java_opts="#{ Regexp.escape("-Djava.awt.headless=true") } #{ Regexp.escape("-Djenkins.install.runSetupWizard=false") }"$/) }
-    its(:content) { should match(/^jenkins_args="--prefix=\/jenkins --webroot=#{ Regexp.escape("/usr/local/jenkins/war") } --httpPort=8180"$/) }
+    its(:content) do
+      should match(
+        /^jenkins_java_opts="#{Regexp.escape(jenkins_java_opts)}"$/
+      )
+    end
+    its(:content) do
+      should match(
+        /^jenkins_args=#{Regexp.escape(jenkins_args)}$/
+      )
+    end
   end
-when "ubuntu"
-  describe file("/etc/default/jenkins") do
+when 'ubuntu'
+  describe file('/etc/default/jenkins') do
     it { should be_file }
-    its(:content) { should match(/^JAVA_ARGS="#{ Regexp.escape("-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false") }"$/) }
+    its(:content) do
+      should match(
+        /^JAVA_ARGS="#{ Regexp.escape(jenkins_java_opts) }"$/
+      )
+    end
     its(:content) { should match(/^HTTP_PORT="8080"$/) }
-    its(:content) { should match(/^JENKINS_ARGS="--webroot=#{ Regexp.escape("/var/cache/$NAME/war") } --httpPort=\$HTTP_PORT --prefix=\/jenkins\s*"$/) }
+    its(:content) do
+      should match(
+        /^JENKINS_ARGS="#{Regexp.escape(jenkins_args)}\s*"$/
+      )
+    end
   end
-when "redhat"
-  describe file("/etc/sysconfig/jenkins") do
+when 'redhat'
+  describe file('/etc/sysconfig/jenkins') do
     it { should be_file }
     its(:content) { should match(/^JENKINS_HOME="#{ Regexp.escape(home) }"$/) }
     its(:content) { should match(/^JENKINS_USER="jenkins"$/) }
-    its(:content) { should match(/^JENKINS_JAVA_OPTIONS="#{ Regexp.escape("-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false") }"$/) }
+    its(:content) do
+      should match(
+        /^JENKINS_JAVA_OPTIONS="#{ Regexp.escape(jenkins_java_opts) }"$/
+      )
+    end
     its(:content) { should match(/^JENKINS_PORT="8080"$/) }
-    its(:content) { should match(/^JENKINS_ARGS="--prefix=\/jenkins\s+"$/) }
+    its(:content) { should match(%r{^JENKINS_ARGS="--prefix=/jenkins\s+"$}) }
   end
 end
 
@@ -76,54 +113,57 @@ describe file(cli) do
   it { should be_mode 755 }
 end
 
-describe file("#{ home }/init.groovy.d") do
+describe file("#{home}/init.groovy.d") do
   it { should be_directory }
   it { should be_mode 755 }
 end
 
-describe file("#{ home }/.ssh") do
+describe file("#{home}/.ssh") do
   it { should be_directory }
   it { should be_mode 700 }
   it { should be_owned_by user }
   it { should be_grouped_into group }
 end
 
-describe file("#{ home }/.ssh/id_rsa") do
+describe file("#{home}/.ssh/id_rsa") do
   it { should be_file }
   it { should be_mode 600 }
   it { should be_owned_by user }
   it { should be_grouped_into group }
 end
 
-describe file("#{ home }/.ssh/id_rsa.pub") do
+describe file("#{home}/.ssh/id_rsa.pub") do
   it { should be_file }
   it { should be_mode 644 }
   it { should be_owned_by user }
   it { should be_grouped_into group }
 end
 
-describe command("ssh-keygen -lf #{ home }/.ssh/id_rsa") do
+describe command("ssh-keygen -lf #{home}/.ssh/id_rsa") do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(/^#{Regexp.escape(ssh_checksum)}$/) }
   its(:stderr) { should match(/^$/) }
 end
 
-describe command("ssh-keygen -lf #{ home }/.ssh/id_rsa.pub") do
+describe command("ssh-keygen -lf #{home}/.ssh/id_rsa.pub") do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(/^#{Regexp.escape(ssh_checksum)}$/) }
   its(:stderr) { should match(/^$/) }
 end
 
-describe command("ssh-keygen -p -P #{ssh_passphrase} -N #{ssh_passphrase} -f #{ home }/.ssh/id_rsa") do
+describe command(
+  "ssh-keygen -p -P #{ssh_passphrase} -N #{ssh_passphrase} -f\
+ #{home}/.ssh/id_rsa"
+) do
   its(:exit_status) { should eq 0 }
 end
 
-describe file("#{ home }/updates") do
+describe file("#{home}/updates") do
   it { should be_directory }
   it { should be_mode 755 }
 end
 
-describe file("#{ home }/updates/default.json") do
+describe file("#{home}/updates/default.json") do
   it { should be_file }
   it { should be_mode 644 }
   it { should be_owned_by user }
@@ -131,12 +171,12 @@ describe file("#{ home }/updates/default.json") do
 end
 
 plugins.each do |p|
-  describe file("#{ home }/plugins/#{ p }.jpi") do
+  describe file("#{home}/plugins/#{p}.jpi") do
     it { should be_file }
   end
 end
 
-describe command("java -jar #{ cli } -s #{ url } list-plugins") do
+describe command("java -jar #{cli} -s #{url} list-plugins") do
   its(:exit_status) { should eq 0 }
   plugins.each do |p|
     its(:stdout) { should match(/^#{ p }\s+/) }
