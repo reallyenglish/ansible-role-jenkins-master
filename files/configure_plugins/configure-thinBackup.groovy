04c4068a32b8ee164c273f399d61e2f00d3e6ae1
Mitsuru Y <mitsuruy@reallyenglish.com>
2017-10-31 15:44:34 +0900

    get the current configuration of thinBackup

--- files/configure_plugins/configure-thinBackup.groovy
+++ files/configure_plugins/configure-thinBackup.groovy
@@ -1,31 +1,71 @@
 import jenkins.model.*
+import groovy.json.*
 
-def plugins = jenkins.model.Jenkins.instance.getPluginManager().getPlugins()
-
-
-def Jenkins jenkins_instance = Jenkins.getInstance()
-
-def thin
-
-plugins.each {
-  println it
-  if (it.shortName == "thinBackup")
-  {
-  thin = it.getPlugin()
-  }
+def plugins() {
+  return jenkins.model.Jenkins.instance.pluginManager.plugins
 }
 
-println ""
+def find_plugin(name) {
+  return plugins().find { it.shortName == name }.plugin
+}
 
-// println thin.getFullBackupSchedule()
-println "before"
-println thin.fullBackupSchedule
+def all_methods(object) {
+  return object.metaClass.methods*.name.sort().unique()
+}
 
-thin.fullBackupSchedule = "H 0 * * 1-5"
-println "after"
-println thin.fullBackupSchedule
-println ""
+def find_intersect(list, prefix1, prefix2) {
+  stem1 = list.findAll { it ==~ "^${prefix1}.*" }
+              .collect { it.replaceFirst(prefix1, "") }
+  stem2 = list.findAll { it ==~ "^${prefix2}.*" }
+              .collect { it.replaceFirst(prefix2, "") }
+  return stem1.toSet().intersect(stem2.toSet())
+}
 
-println thin.metaClass.methods*.name.sort().unique()
+def configurables(plugin) {
+  return find_intersect(all_methods(plugin), "get", "set")
+}
 
-// [configure, doCheckBackupPath, doCheckBackupSchedule, doCheckExcludedFilesRegex, doCheckForceQuietModeTimeout, doCheckWaitForIdle, doDynamic, equals, getBackupAdditionalFilesRegex, getBackupPath, getClass, getDiffBackupSchedule, getExcludedFilesRegex, getExpandedBackupPath, getForceQuietModeTimeout, getFullBackupSchedule, getHudsonHome, getInstance, getNrMaxStoredFull, getWrapper, hashCode, isBackupAdditionalFiles, isBackupBuildArchive, isBackupBuildResults, isBackupBuildsToKeepOnly, isBackupNextBuildNumber, isBackupPluginArchives, isBackupUserContents, isCleanupDiff, isMoveOldBackupsToZipFile, isWaitForIdle, notify, notifyAll, postInitialize, save, setBackupAdditionalFiles, setBackupAdditionalFilesRegex, setBackupBuildArchive, setBackupBuildResults, setBackupBuildsToKeepOnly, setBackupNextBuildNumber, setBackupPath, setBackupPluginArchives, setBackupUserContents, setCleanupDiff, setDiffBackupSchedule, setExcludedFilesRegex, setForceQuietModeTimeout, setFullBackupSchedule, setMoveOldBackupsToZipFile, setNrMaxStoredFull, setNrMaxStoredFullAsString, setServletContext, setWaitForIdle, start, stop, toString, wait]
+def flags(plugin) {
+  return find_intersect(all_methods(plugin), "is", "set")
+}
+
+def get_config(plugin) {
+  def json = new JsonBuilder()
+  def config = [:]
+  configurables(plugin).each {
+    value = plugin."get$it"()
+    config["${it}"] = value
+  }
+  flags(plugin).each {
+    value = plugin."is$it"()
+    config["${it}"] = value
+  }
+  json config
+  return json
+}
+
+def set_config(plugin, config) {
+}
+
+if (args.length > 0) {
+  switch (args[0]) {
+    case "get" :
+      println get_config(find_plugin("thinBackup"))
+      break
+    case "set" :
+      if (args.length > 1) {
+        set_config(find_plugin("thinBackup"), args[1])
+      }
+      else {
+        println "the second argument is required to configure"
+      }
+      break
+    default :
+      println "the first argument must be 'get' or 'set'"
+      break
+  }
+}
+else {
+  println "argument 'get' or 'set is required"
+}
+// vim: ft=groovy
