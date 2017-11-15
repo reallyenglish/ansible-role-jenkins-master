de23956133e20f7052cab1727e6c861ae3196a55
Mitsuru Y <mitsuruy@reallyenglish.com>
2017-11-15 19:03:30 +0900

    make configure-plugin.groovy support hipchat plugin
    - more plugin can be supported by adding a map
    - remove configure-hipchat.groovy

--- files/configure_plugins/configure-plugin.groovy
+++ files/configure_plugins/configure-plugin.groovy
@@ -1,12 +1,32 @@
 import jenkins.model.*
 import groovy.json.*
+import hudson.model.*
+
+def class_name(name) {
+  // map of pluginname:classname
+  // there is no way to find the descriptor name from the plugin name
+  // without reading the source code.
+  def map = ['hipchat':'HipChatNotifier']
+  return map[name] ? map[name] : ''
+}
+
+def get_descriptor(name) {
+  def Jenkins jenkins_instance = Jenkins.getInstance()
+  def descriptor_name = "jenkins.plugins.${name}.${class_name(name)}"
+  return jenkins_instance.getDescriptor(descriptor_name)
+}
 
 def plugins() {
   return jenkins.model.Jenkins.instance.pluginManager.plugins
 }
 
 def find_plugin(name) {
-  return plugins().find { it.shortName == name }.plugin
+  if (get_descriptor(name) != null) {
+    return get_descriptor(name)
+  }
+  else {
+    return plugins().find { it.shortName == name }.plugin
+  }
 }
 
 def all_methods(object) {
